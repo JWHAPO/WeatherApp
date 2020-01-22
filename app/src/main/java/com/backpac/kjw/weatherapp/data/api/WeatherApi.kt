b09ccf3be8165edea5c7ebbd2d2c751ef70a3417
JWHAPO <kim2101714@gmail.com>
2020-01-22 20:28:53 +0900

    add apis

---
+++ app/src/main/java/com/backpac/kjw/weatherapp/data/api/WeatherApi.kt
@@ -0,0 +1,21 @@
+package com.backpac.kjw.weatherapp.data.api
+
+import com.backpac.kjw.weatherapp.data.domain.Location
+import com.backpac.kjw.weatherapp.data.domain.Weather
+import io.reactivex.Observable
+import retrofit2.http.GET
+import retrofit2.http.Path
+
+/**
+ * WeatherApp
+ * Class: Apis
+ * Created by JEONGWOOKIM on 2020-01-22.
+ * Description: Apis for weather
+ */
+interface WeatherApi{
+    @GET("/api/location/search/?query={query}")
+    fun getLocations(@Path("query") query: String): Observable<Location>
+
+    @GET("/api/location/{woeid}")
+    fun getWeather(@Path("woeid") query: Int): Observable<Weather>
+}
