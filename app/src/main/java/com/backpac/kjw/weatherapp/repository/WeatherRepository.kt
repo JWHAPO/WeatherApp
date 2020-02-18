51d965de9216e69338374f86c636e96c5c6a1ba1
JWHAPO <kim2101714@gmail.com>
2020-02-18 17:35:13 +0900

    add repository package

---
+++ app/src/main/java/com/backpac/kjw/weatherapp/repository/WeatherRepository.kt
@@ -0,0 +1,12 @@
+package com.backpac.kjw.weatherapp.repository
+
+/**
+ * WeatherApp
+ * Class: WeatherRepository
+ * Created by JEONGWOOKIM on 2020-02-18.
+ * Description:
+ */
+
+class WeatherRepository {
+
+}
